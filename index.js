a5794acfe32be9e92a75291234b5d5a1c6b8c779
Nimu93 <63539031+Nimu93@users.noreply.github.com>
2020-06-07 19:01:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 const ms = require("ms");
 const bot = new discord.Client();
 const prefix = "*"
-bot.login("NzE2NTk2NzAwMzgyNDk0Nzcx.XtOFFQ.FsY3BKc6iBatp7Gie5W2dq2PFWo")
+bot.login("NzE2NTk2NzAwMzgyNDk0Nzcx.Xt0dQA.CctnQHC-xref6cBm6BfwndAZ2_o")
 
 bot.on("ready", () =>{
 bot.user.setActivity("recruter!", {type:"PLAYING", name:"test"})
